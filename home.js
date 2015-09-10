ef2c68ef9e13686dfa206fee2c4b1823ed71dfa1
nighttime <softwareatnight@gmail.com>
2015-09-10 16:30:52 -0400

    added real content

---
+++ home.js
@@ -0,0 +1,32 @@
+imageExposureNormal = 0.3;
+imageExposureSmall = 0.2;
+smallSizeThresholdPx = 600;
+
+$(document).ready(function () {
+
+//    var img = $('#header-image')[0];
+//
+//    $('<img/>').attr('src', $(img).attr('src')).load(function () {
+//        $('#header-title').css({
+//            'margin-top': this.height * 0.3 + 'px'
+//        }).show();
+//    });
+
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing');
+    });
+
+});
+
+//function positionHeader() {
+//    var imageHeight = $('#header-title').height();
+//    
+//}
