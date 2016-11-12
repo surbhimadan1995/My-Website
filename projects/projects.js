ad6de4fff1bb11f5b7714faccc0181f40f62ea5a
nighttime <softwareatnight@gmail.com>
2016-11-12 16:41:33 -0500

    small tweaks

--- projects/projects.js
+++ projects/projects.js
@@ -8,13 +8,6 @@ projects = [
         <a class="contact-link" href="https://vimeo.com/138032443">Check it out in action!</a>`
     },
     {
-        pics: ['digital_bear.png', 'digital_wolf.png'],
-        title: 'wild things',
-        body: `This project stemmed from a mental block. I needed a way to free my mind up from day-to-day tasks and feel creative again.
-        The solution was to focus on a constructive, yet spirited goal.
-        Now when I'm feeling stifled, I'll work on illustrating an animal as part of this collection.`
-    },
-    {
         pics: ['tti.png'],
         title: 'Two to Infinity',
         body: `2048: Two to Infinity began with a simple mission: to redo 2048 with <em>style</em>.
@@ -22,12 +15,10 @@ projects = [
         To date, Two to Infinity has 11k downloads. You can <a class="contact-link" href="https://www.youtube.com/watch?v=DarccjOP6zE">watch</a> the trailer and <a class="contact-link" href="https://itunes.apple.com/us/app/beyond-2048-two-to-infinity!/id860372644?mt=8">play</a> it yourself.`
     },
     {
-        pics: ['echo.png'],
-        title: 'echo',
-        body: `What makes a shareable idea?
-Echo is an experiment in meme propagation. It models the way an idea spreads through people using one-way messages.
-It starts when a user shares a thought. Another user around the world reads it, and if they share it, it's read by another user, and another, and another, until someone breaks the chain. Then no one will see the message again.
-Echo is an iOS project with a web companion. I conceived the idea, directed project-wide style, and engineered the mobile client.`
+        pics: ['digital_bear.png', 'digital_wolf.png'],
+        title: 'wild things',
+        body: `I enjoy digital illustration in addition to interface design.
+        This series is an ongoing project that I add to from time to time.`
     },
     {
         pics: ['growing.png'],
@@ -37,5 +28,13 @@ Echo is an iOS project with a web companion. I conceived the idea, directed proj
         I wanted to explore organic motion dictated entirely by computation. Every aspect of the scene is generated probabilistically, which means that each tree is unique.
         The leaves were painted and scanned by my partner. I conceived the concept and programmed the scene in Processing, carefully tuning and easing by hand to achieve motion that truly feels alive.
         Check out the source code <a class="contact-link" href="https://github.com/nighttime/growing">here</a>.`
+    },
+    {
+        pics: ['echo.png'],
+        title: 'echo',
+        body: `What makes a shareable idea?
+Echo is an experiment in meme propagation. It models the way an idea spreads through people using one-way messages.
+It starts when a user shares a thought. Another user around the world reads it, and if they share it, it's read by another user, and another, and another, until someone breaks the chain. Then no one will see the message again.
+Echo is an iOS project with a web companion. I conceived the idea, directed project-wide style, and engineered the mobile client.`
     }
 ]
